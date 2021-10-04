7b3036ecb8b4016066c8bbdc5fd1bb44aba12ce2
IKrylov <IKrylov@lanit.ru>
2021-10-04 16:15:50 +0400

    init

---
+++ src/test/java/org/example/api/store/StoreApiTest.java
@@ -0,0 +1,17 @@
+package org.example.api.store;
+
+import org.testng.annotations.Test;
+
+public class StoreApiTest {
+    @Test
+    public void placeOrderTest() {
+        // todo: офрмить заказ на питомца
+        // todo: найти оформленный заказ
+    }
+
+    @Test
+    public void deleteOrderTest() {
+        // todo: удалить заказ
+        // todo: проверить удаление заказа
+    }
+}
