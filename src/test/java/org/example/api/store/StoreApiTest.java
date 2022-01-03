fd3e235015a66e896c210767f58975bdc92407b6
vilgame <vilgame@mail.ru>
2022-01-04 02:28:02 +0300

    add checkObjectSave

--- src/test/java/org/example/api/store/StoreApiTest.java
+++ src/test/java/org/example/api/store/StoreApiTest.java
@@ -1,4 +1,59 @@
 package org.example.api.store;
 
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import org.example.store.Order;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Random;
+
+import static io.restassured.RestAssured.given;
+
 public class StoreApiTest {
+
+    @BeforeClass
+    public void prepare() throws IOException {
+        System.getProperties().load(ClassLoader.getSystemResourceAsStream("my.properties"));
+        RestAssured.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri("https://petstore.swagger.io/v2/")
+                .addHeader("api_key", System.getProperty("api.key"))
+                .setAccept(ContentType.JSON)
+                .setContentType(ContentType.JSON)
+                .log(LogDetail.ALL)
+                .build();
+        RestAssured.filters(new ResponseLoggingFilter());
+    }
+
+    @Test
+    public void checkObjectSave() {
+        Order order = new Order();
+        int orderId = new Random().nextInt(1000);
+        int petId = new Random().nextInt(1000);
+        order.setId(orderId);
+        order.setPetId(petId);
+        given()
+                .body(order)
+                .when()
+                .post("/store/order")
+                .then()
+                .statusCode(200);
+
+        Order actual =
+                given()
+                        .pathParam("orderId", orderId)
+                        .when()
+                        .get("/store/order/{orderId}")
+                        .then()
+                        .statusCode(200)
+                        .extract().body()
+                        .as(Order.class);
+        Assert.assertEquals(actual.getId(), order.getId());
+    }
+
 }
